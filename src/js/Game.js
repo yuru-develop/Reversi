9825a388cb5f9e88e5795ff50c390e01dcd1f923
shown <shown22327989@gmail.com>
2022-12-26 00:30:29 +0900

    startGame() method

--- src/js/Game.js
+++ src/js/Game.js
@@ -1,6 +1,7 @@
 export default class Game{
   #table = []; 
   constructor(){
+    //tableのリセット
     for(let i = 0 ;i < 8; i++){
       this.#table.push([])
       for(let j = 0;j < 8; j++){
@@ -8,6 +9,14 @@ export default class Game{
       }
     }
   } 
+  //デフォルトの石の配置
+  startGame(){
+    this.#table[3][3]="Black";
+    this.#table[4][4]="Black";
+    this.#table[3][4]="White";
+    this.#table[4][3]="White";
+  }
+  //テスト表示用関数
   Test(){
     console.log(this.#table);
   }

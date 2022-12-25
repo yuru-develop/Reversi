0536bf50e528684523dcbcf388e3423115929c7e
sherkerpnd <95730605+sherkerpnd@users.noreply.github.com>
2022-12-25 23:31:19 +0900

    field,init()作成

--- src/js/Player.js
+++ src/js/Player.js
@@ -1,3 +1,28 @@
 export default class Player{
-  
+  #myColorIsBlack = true;
+  #canPutPlaces = [];
+  #count = 2;
+  constructor(brackOrWhite){
+    this.init(brackOrWhite);
+  };
+
+  refleshCanPutPlaces(){
+    for(let i = 0; i < 8; i++){
+        for(let j = 0; j < 8; j++){
+
+        }
+    }
+  }
+  countMyColor(){
+    for(let i = 0; i < 8; i++){
+        for(let j = 0; j < 8; j++){
+            
+        }
+    }
+  }
+  init(brackOrWhite){
+    brackOrWhite === "Brack"?this.#myColorIsBlack = true:this.#myColorIsBlack = false;
+    this.#canPutPlaces = this.refleshCanPutPlaces();
+    this.#count = this.countMyColor();
+  }
 }
